ff8fe96917fa1cfc12b8a29821b7b0617af99b86
unknown <eric.huie@mail.mcgill.ca>
2017-10-31 15:23:03 -0400

    added comments for GameController, Light/Ultra Poller, Odometer, OdometryDisplay, UltrasonicController

--- src/ca/mcgill/ecse211/captureflag/UltrasonicPoller.java
+++ src/ca/mcgill/ecse211/captureflag/UltrasonicPoller.java
@@ -2,6 +2,10 @@ package ca.mcgill.ecse211.captureflag;
 
 import lejos.robotics.SampleProvider;
 
+/*
+ * This class will instantiate a poller thread and run it so that we can get data
+ * samples from the ultrasonic sensor
+ */
 public class UltrasonicPoller extends Thread {
 	private SampleProvider us;
 	private UltrasonicLocalization ul;
@@ -28,6 +32,7 @@ public class UltrasonicPoller extends Thread {
 		}
 	}
 	
+	//Allows thread to be stopped without deadlock
 	protected void killTask() {
 		kill = true;
 	}
