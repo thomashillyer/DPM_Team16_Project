9f26b122143e3e44485f6d0a79b27db7fe91971a
thomashillyer <thomashillyer@users.noreply.github.com>
2017-11-08 23:49:36 -0500

    Attempt to implement wifi class that gets data

--- src/ca/mcgill/ecse211/captureflag/CaptureFlag.java
+++ src/ca/mcgill/ecse211/captureflag/CaptureFlag.java
@@ -14,9 +14,11 @@ import lejos.robotics.RegulatedMotorListener;
 import lejos.robotics.SampleProvider;
 
 /**
- * CaptureFlag is the main class that instantiates all of the individual units that will be needed to operate the robot. 
- * It also has constants that are used in the other classes of the project, as well as static methods. 
- * This is to make sure that setting any value is consistent among all classes, and that certain methods are not copied over into all of the other classes.
+ * CaptureFlag is the main class that instantiates all of the individual units
+ * that will be needed to operate the robot. It also has constants that are used
+ * in the other classes of the project, as well as static methods. This is to
+ * make sure that setting any value is consistent among all classes, and that
+ * certain methods are not copied over into all of the other classes.
  */
 
 public class CaptureFlag {
@@ -38,10 +40,9 @@ public class CaptureFlag {
 
 	protected static final int ROTATIONSPEED = 100;
 
-	protected static final int ACCELERATION = 500; //1000
+	protected static final int ACCELERATION = 500; // 1000
 	protected static final int NAV_ACCELERATION = 100;
 	protected static final int FORWARDSPEED = 150;
-	
 
 	private static final int FILTER_OUT = 23;
 	private static int filterControl;
@@ -49,11 +50,11 @@ public class CaptureFlag {
 	protected static final double BOT_LENGTH = 14.3;
 
 	protected static final double TILE_LENGTH = 30.48;
-	
+
 	private static final String SERVER_IP = "192.168.2.37";
-    private static final int TEAM_NUMBER = 1;
-    // Enable/disable printing of debug info from the WiFi class
-    private static final boolean ENABLE_DEBUG_WIFI_PRINT = true;
+	private static final int TEAM_NUMBER = 16;
+	// Enable/disable printing of debug info from the WiFi class
+	private static final boolean ENABLE_DEBUG_WIFI_PRINT = true;
 
 	private static int x0 = 0;
 	private static int y0 = 0;
@@ -62,21 +63,21 @@ public class CaptureFlag {
 	private static int corner = 0;
 
 	public static void main(String[] args) {
-		//set up screen for use
+		// set up screen for use
 		int option = 0;
 		final TextLCD screen = LocalEV3.get().getTextLCD();
-		
-		//set up us sensor
+
+		// set up us sensor
 		SensorModes usSensor = new EV3UltrasonicSensor(usPort); // usSensor is the instance
-		SampleProvider usDistance = usSensor.getMode("Distance"); // usDistance provides samples from												// this instance
+		SampleProvider usDistance = usSensor.getMode("Distance"); // usDistance provides samples from // this instance
 		float[] usData = new float[usDistance.sampleSize()]; // usData is the buffer in which data are
-		
-		//set up color sensor
+
+		// set up color sensor
 		SensorModes colorSamplerSensor = new EV3ColorSensor(lightSampler);
 		SampleProvider colorSensorValue = colorSamplerSensor.getMode("Red");
 		float[] colorSensorData = new float[colorSamplerSensor.sampleSize()];
 
-		//instantiate classes
+		// instantiate classes
 		Odometer odometer = new Odometer(leftMotor, rightMotor);
 		OdometryDisplay odoDispl = new OdometryDisplay(odometer, screen);
 		Navigation nav = new Navigation(odometer, leftMotor, rightMotor);
@@ -84,42 +85,45 @@ public class CaptureFlag {
 		FlagDetection flag = new FlagDetection(leftMotor, rightMotor, odometer, nav, 0, 0, 1);
 		UltrasonicPoller usPoller = new UltrasonicPoller(usSensor, usData, usLocal, flag);
 		// Initialize WifiConnection class
-        WifiConnection conn = new WifiConnection(SERVER_IP, TEAM_NUMBER, ENABLE_DEBUG_WIFI_PRINT);
+		WifiConnection conn = new WifiConnection(SERVER_IP, TEAM_NUMBER, ENABLE_DEBUG_WIFI_PRINT);
 		do {
 			printMenu();
 			option = Button.waitForAnyPress();
 		} while (option != Button.ID_LEFT && option != Button.ID_RIGHT); // and wait for a button press. The button
 
-		//pass points to light localization
-		//TODO the following code is vestigial, game data will be retrieved from the server. Remove it to make way for getting data from server. 
+		// pass points to light localization
+		// TODO the following code is vestigial, game data will be retrieved from the
+		// server. Remove it to make way for getting data from server.
 		int[] points = { x0, y0, xC, yC, corner };
 		LightLocalization lightLocal = new LightLocalization(leftMotor, rightMotor, odometer, nav);
 		LightPoller lp = new LightPoller(colorSensorValue, colorSensorData, lightLocal);
 
-		GameController gc = new GameController(rightMotor, leftMotor, sensorMotor, odometer,lightLocal, usLocal, lp, usPoller, nav, conn, flag);
-		
-		//TODO remove the switch case as it will not be used for the final project. Could maybe stay for testing purposes.
+		GameController gc = new GameController(rightMotor, leftMotor, sensorMotor, odometer, lightLocal, usLocal, lp,
+				usPoller, nav, conn, flag);
+
+		// TODO remove the switch case as it will not be used for the final project.
+		// Could maybe stay for testing purposes.
 		switch (option) {
 		case Button.ID_LEFT:
 			odometer.start();
-//			usPoller.start();
+			// usPoller.start();
 			odoDispl.start();
-//			screen.clear();
-//			usLocal.start();
-//			lightLocal.start();
-//			lp.start();
+			// screen.clear();
+			// usLocal.start();
+			// lightLocal.start();
+			// lp.start();
 			gc.start();
 			t.clear();
 			break;
 		case Button.ID_RIGHT:
-//			UltrasonicPoller usPoll = new UltrasonicPoller(usSensor, usData, usLocal);
-//			odometer.start();
-//			usPoll.start();
-//			odoDispl.start();
-//			screen.clear();
-//			usLocal.start();
-//			Button.waitForAnyPress();
-//			lightLocal.start();
+			// UltrasonicPoller usPoll = new UltrasonicPoller(usSensor, usData, usLocal);
+			// odometer.start();
+			// usPoll.start();
+			// odoDispl.start();
+			// screen.clear();
+			// usLocal.start();
+			// Button.waitForAnyPress();
+			// lightLocal.start();
 			break;
 		default:
 			System.out.println("Error - invalid button"); // None of the above - abort
@@ -127,59 +131,68 @@ public class CaptureFlag {
 			break;
 		}
 
-		//escape at any point during program execution if escape button is pressed
-		while (Button.waitForAnyPress() != Button.ID_ESCAPE);
+		// escape at any point during program execution if escape button is pressed
+		while (Button.waitForAnyPress() != Button.ID_ESCAPE)
+			;
 		System.exit(0);
 	}
 
 	/**
-	 * Prints to the screen a menu for setting the travel to points for the ZipLine lab, as well as a menu for choosing either rising or falling edge localization.
+	 * Prints to the screen a menu for setting the travel to points for the ZipLine
+	 * lab, as well as a menu for choosing either rising or falling edge
+	 * localization.
 	 */
 	private static void printMenu() {
 		boolean enter = false;
 		t.clear();
 		int counter = 0;
 
-//		x0 = printXY("X0: ", 0, 0);
-//		y0 = printXY("Y0: ", 0, 1);
-//		xC = printXY("XC: ", 0, 2);
-//		yC = printXY("YC: ", 0, 3);
+		// x0 = printXY("X0: ", 0, 0);
+		// y0 = printXY("Y0: ", 0, 1);
+		// xC = printXY("XC: ", 0, 2);
+		// yC = printXY("YC: ", 0, 3);
 
-//		enter = false;
-//		counter = 0;
-//		t.drawString("Corner: ", 0, 4);
-//		while (!enter) {
-//			int buttonPressed = Button.waitForAnyPress();
-//			if (buttonPressed != Button.ID_ENTER) {
-//				if (buttonPressed == Button.ID_DOWN && counter == 0) {
-//					counter = 0;
-//				} else if (buttonPressed == Button.ID_DOWN && counter > 0) {
-//					counter--;
-//				} else if (buttonPressed == Button.ID_UP && counter == 3) {
-//					counter = 3;
-//				} else if (buttonPressed == Button.ID_UP && counter < 3) {
-//					counter++;
-//				}
-//				t.drawInt(counter, 8, 4);
-//			} else {
-//				corner = counter;
-//				enter = true;
-//			}
-//		}
-//
-//		t.clear(); // the screen at initialization
+		// enter = false;
+		// counter = 0;
+		// t.drawString("Corner: ", 0, 4);
+		// while (!enter) {
+		// int buttonPressed = Button.waitForAnyPress();
+		// if (buttonPressed != Button.ID_ENTER) {
+		// if (buttonPressed == Button.ID_DOWN && counter == 0) {
+		// counter = 0;
+		// } else if (buttonPressed == Button.ID_DOWN && counter > 0) {
+		// counter--;
+		// } else if (buttonPressed == Button.ID_UP && counter == 3) {
+		// counter = 3;
+		// } else if (buttonPressed == Button.ID_UP && counter < 3) {
+		// counter++;
+		// }
+		// t.drawInt(counter, 8, 4);
+		// } else {
+		// corner = counter;
+		// enter = true;
+		// }
+		// }
+		//
+		// t.clear(); // the screen at initialization
 		t.drawString("left = fallingEdge", 0, 0);
 		t.drawString("right = risingEdge", 0, 1);
 	}
 
 	/**
-	 * Prints to the screen a menu for setting the travel to points for the ZipLine lab.
-	 * For setting the points, the used can push the up or down buttons to change the value of the displayed value.
-	 * When the user wants submit the value, the enter button should be pressed.
-	 * @param disp The string to display to the LCD screen
-	 * @param x The x position on the screen to display the string
-	 * @param y The y position on the screen to display the string
-	 * @return An integer that represents what the user has chosen for the displayed value.
+	 * Prints to the screen a menu for setting the travel to points for the ZipLine
+	 * lab. For setting the points, the used can push the up or down buttons to
+	 * change the value of the displayed value. When the user wants submit the
+	 * value, the enter button should be pressed.
+	 * 
+	 * @param disp
+	 *            The string to display to the LCD screen
+	 * @param x
+	 *            The x position on the screen to display the string
+	 * @param y
+	 *            The y position on the screen to display the string
+	 * @return An integer that represents what the user has chosen for the displayed
+	 *         value.
 	 */
 	public static int printXY(String disp, int x, int y) {
 		boolean enter = false;
@@ -205,47 +218,58 @@ public class CaptureFlag {
 		}
 		return counter;
 	}
-	
+
 	/**
 	 * 
-	 * @param distance A distance collected from sampling the ultrasonic sensor.
+	 * @param distance
+	 *            A distance collected from sampling the ultrasonic sensor.
 	 * @return A filtered value
 	 */
 	protected static int filter(int distance) {
-		  if (distance >= 255  && filterControl < FILTER_OUT) {
-		      // bad value, do not set the distance var, however do increment the
-		      // filter value
-		      filterControl++;
-		      return distance;
-		    } else if (distance >= 255) {
-		      // We have repeated large values, so there must actually be nothing
-		      // there: leave the distance alone
-		      return distance;
-		    } else {
-		      // distance went below 255: reset filter and leave
-		      // distance alone.
-		      filterControl = 0;
-		      return distance;
-		    }
-	  }
+		if (distance >= 255 && filterControl < FILTER_OUT) {
+			// bad value, do not set the distance var, however do increment the
+			// filter value
+			filterControl++;
+			return distance;
+		} else if (distance >= 255) {
+			// We have repeated large values, so there must actually be nothing
+			// there: leave the distance alone
+			return distance;
+		} else {
+			// distance went below 255: reset filter and leave
+			// distance alone.
+			filterControl = 0;
+			return distance;
+		}
+	}
 
 	/**
-	 * This method converts a distance into a number of rotations the motor should complete to go the distance.
-	 * @param radius The radius of the robots wheel.
-	 * @param distance The desired distance that the robot should travel.
-	 * @return An integer representing the amount of rotations the robot should turn to go the given distance.
+	 * This method converts a distance into a number of rotations the motor should
+	 * complete to go the distance.
+	 * 
+	 * @param radius
+	 *            The radius of the robots wheel.
+	 * @param distance
+	 *            The desired distance that the robot should travel.
+	 * @return An integer representing the amount of rotations the robot should turn
+	 *         to go the given distance.
 	 */
 	protected static int convertDistance(double radius, double distance) {
 		return (int) ((180.0 * distance) / (Math.PI * radius));
 	}
 
 	/**
-	 * This method converts a given angle into a number of rotations the motor should complete to rotate the angle.
-	 * Uses the convertDistance method.
-	 * @param radius The radius of the robots wheel.
-	 * @param TRACK The track length, in cm, of the robot.
-	 * @param angle The angle that should be turned, in degrees.
-	 * @return An integer representing the amount of rotations the robot should turn to go turn the given angle.
+	 * This method converts a given angle into a number of rotations the motor
+	 * should complete to rotate the angle. Uses the convertDistance method.
+	 * 
+	 * @param radius
+	 *            The radius of the robots wheel.
+	 * @param TRACK
+	 *            The track length, in cm, of the robot.
+	 * @param angle
+	 *            The angle that should be turned, in degrees.
+	 * @return An integer representing the amount of rotations the robot should turn
+	 *         to go turn the given angle.
 	 */
 	protected static int convertAngle(double radius, double TRACK, double angle) {
 		return convertDistance(radius, Math.PI * TRACK * angle / 360.0);
--- src/ca/mcgill/ecse211/captureflag/GameController.java
+++ src/ca/mcgill/ecse211/captureflag/GameController.java
@@ -9,71 +9,72 @@ import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.robotics.RegulatedMotor;
 
 /**
- * This class will implement the overall game logic that will follow according to our software flowchart
+ * This class will implement the overall game logic that will follow according
+ * to our software flowchart
  */
 public class GameController extends Thread {
-	
-    protected static EV3LargeRegulatedMotor leftMotor;
-    protected static EV3LargeRegulatedMotor rightMotor;
-    private static EV3MediumRegulatedMotor zip;
-  
+
+	protected static EV3LargeRegulatedMotor leftMotor;
+	protected static EV3LargeRegulatedMotor rightMotor;
+	private static EV3MediumRegulatedMotor zip;
+
 	private LightLocalization li;
 	private UltrasonicLocalization us;
 	private LightPoller lp;
 	private UltrasonicPoller usPoller;
 	private Navigation nav;
-	private WifiConnection conn;
+	static WifiConnection conn;
 	private Odometer odo;
-	
-//	private EV3LargeRegulatedMotor[] syncList = new EV3LargeRegulatedMotor[1];
+	private Wifi wifi;
+
+	// private EV3LargeRegulatedMotor[] syncList = new EV3LargeRegulatedMotor[1];
 
 	private FlagDetection flag;
 
-	
-	//all game variables
-	int redTeam;   //team number starting from red
-	int greenTeam; //team number starting from green 
-	int redCorner; //red team starting corner
-	int greenCorner; //green team starting corner
-	int greenOpFlag; //green opponent flag color
-	int redOpFlag; //red opponent flag color
-	int red_ll_x;  //lower left hand corner of red zone x
-	int red_ll_y;  //lower left hand corner of red zone y
-	int red_ur_x;  //upper right hand corner of red zone x
-	int red_ur_y;  //upper right hand corner of red zone y
-	int green_ll_x; //lower left hand corner of green zone x
-	int green_ll_y; //lower left hand corner of green zone y
-	int green_ur_x; //upper right hand corner of green zone x
-	int green_ur_y; //upper right hand corner of green zone y
-	int zc_r_x; //end point corresponding to zip line in red zone x
-	int zc_r_y; //end point corresponding to zip line in red zone y
-	int zo_r_x; //end point together with ZC_R indicates direction of zip line x
-	int zo_r_y; //end point together with ZC_R indicates direction of zip line y
-	int zc_g_x; //end point corresponding to zip line in green zone x
-    int zc_g_y; //end point corresponding to zip line in green zone y
-    int zo_g_x; //end point together with ZC_G indicates direction of zip line x
-    int zo_g_y; //end point together with ZC_G indicates direction of zip line y
-    int sh_ll_x; //lower left hand corner of horizontal shallow water zone x
-    int sh_ll_y; //lower left hand corner of horizontal shallow water zone y
-    int sh_ur_x; //upper right hand corner of horizontal shallow water zone x
-    int sh_ur_y; //upper right hand corner of horizontal shallow water zone y
-    int sv_ll_x; //lower left hand corner of vertical shallow water zone x
-    int sv_ll_y; //lower left hand corner of vertical shallow water zone y
-    int sv_ur_x; //upper right hand corner of vertical shallow water zone x
-    int sv_ur_y; //upper right hand corner of vertical shallow water zone y
-    int sr_ll_x; //lower left hand corner of search region in red player zone x
-    int sr_ll_y; //lower left hand corner of search region in red player zone y
-    int sr_ur_x; //upper right hand corner of search region in red player zone x
-    int sr_ur_y; //upper right hand corner of search region in red player zone y
-    int sg_ll_x; //lower left hand corner of search region in green player zone x
-    int sg_ll_y; //lower left hand corner of search region in green player zone y
-    int sg_ur_x; //upper right hand corner of search region in green player zone x
-    int sg_ur_y; //upper right hand corner of search region in green player zone y
-    
-    
-	//constructor
-	public GameController(EV3LargeRegulatedMotor rightMotor, EV3LargeRegulatedMotor leftMotor, EV3MediumRegulatedMotor zip, Odometer odo, LightLocalization li, 
-	    UltrasonicLocalization us, LightPoller lp, UltrasonicPoller usPoller, Navigation nav, WifiConnection conn,FlagDetection flag) {
+	// all game variables
+	static int redTeam; // team number starting from red
+	static int greenTeam; // team number starting from green
+	static int redCorner; // red team starting corner
+	static int greenCorner; // green team starting corner
+	static int greenOpFlag; // green opponent flag color
+	static int redOpFlag; // red opponent flag color
+	static int red_ll_x; // lower left hand corner of red zone x
+	static int red_ll_y; // lower left hand corner of red zone y
+	static int red_ur_x; // upper right hand corner of red zone x
+	static int red_ur_y; // upper right hand corner of red zone y
+	static int green_ll_x; // lower left hand corner of green zone x
+	static int green_ll_y; // lower left hand corner of green zone y
+	static int green_ur_x; // upper right hand corner of green zone x
+	static int green_ur_y; // upper right hand corner of green zone y
+	static int zc_r_x; // end point corresponding to zip line in red zone x
+	static int zc_r_y; // end point corresponding to zip line in red zone y
+	static int zo_r_x; // end point together with ZC_R indicates direction of zip line x
+	static int zo_r_y; // end point together with ZC_R indicates direction of zip line y
+	static int zc_g_x; // end point corresponding to zip line in green zone x
+	static int zc_g_y; // end point corresponding to zip line in green zone y
+	static int zo_g_x; // end point together with ZC_G indicates direction of zip line x
+	static int zo_g_y; // end point together with ZC_G indicates direction of zip line y
+	static int sh_ll_x; // lower left hand corner of horizontal shallow water zone x
+	static int sh_ll_y; // lower left hand corner of horizontal shallow water zone y
+	static int sh_ur_x; // upper right hand corner of horizontal shallow water zone x
+	static int sh_ur_y; // upper right hand corner of horizontal shallow water zone y
+	static int sv_ll_x; // lower left hand corner of vertical shallow water zone x
+	static int sv_ll_y; // lower left hand corner of vertical shallow water zone y
+	static int sv_ur_x; // upper right hand corner of vertical shallow water zone x
+	static int sv_ur_y; // upper right hand corner of vertical shallow water zone y
+	static int sr_ll_x; // lower left hand corner of search region in red player zone x
+	static int sr_ll_y; // lower left hand corner of search region in red player zone y
+	static int sr_ur_x; // upper right hand corner of search region in red player zone x
+	static int sr_ur_y; // upper right hand corner of search region in red player zone y
+	static int sg_ll_x; // lower left hand corner of search region in green player zone x
+	static int sg_ll_y; // lower left hand corner of search region in green player zone y
+	static int sg_ur_x; // upper right hand corner of search region in green player zone x
+	static int sg_ur_y; // upper right hand corner of search region in green player zone y
+
+	// constructor
+	public GameController(EV3LargeRegulatedMotor rightMotor, EV3LargeRegulatedMotor leftMotor,
+			EV3MediumRegulatedMotor zip, Odometer odo, LightLocalization li, UltrasonicLocalization us, LightPoller lp,
+			UltrasonicPoller usPoller, Navigation nav, WifiConnection conn, FlagDetection flag) {
 		this.li = li;
 		this.us = us;
 		this.lp = lp;
@@ -88,101 +89,59 @@ public class GameController extends Thread {
 		this.flag = flag;
 
 	}
+
 	@SuppressWarnings("rawtypes")
 	public void run() {
 
-	  //obtain all game parameters from server and assign them to the appropriate variable
-//	  try {
-//	    Map data = conn.getData();
-//	    
-//	    redTeam = ((Long) data.get("RedTeam")).intValue();
-//	    greenTeam = ((Long) data.get("GreenTeam")).intValue();
-//	    redCorner = ((Long) data.get("RedCorner")).intValue();
-//	    greenCorner = ((Long) data.get("GreenCorner")).intValue();
-//	    greenOpFlag = ((Long) data.get("OG")).intValue(); 
-//	    redOpFlag = ((Long) data.get("OR")).intValue();
-//	    red_ll_x = ((Long) data.get("Red_LL_x")).intValue(); 
-//	    red_ll_y = ((Long) data.get("Red_LL_y")).intValue();
-//	    red_ur_x = ((Long) data.get("Red_UR_x")).intValue(); 
-//	    red_ur_y = ((Long) data.get("Red_UR_y")).intValue();
-//	    green_ll_x = ((Long) data.get("Green_LL_x")).intValue();
-//	    green_ll_y = ((Long) data.get("Green_LL_y")).intValue();
-//	    green_ur_x = ((Long) data.get("Green_LL_x")).intValue(); 
-//	    green_ur_y = ((Long) data.get("Green_LL_y")).intValue(); 
-//	    zc_r_x = ((Long) data.get("ZC_R_x")).intValue();
-//	    zc_r_y = ((Long) data.get("ZC_R_y")).intValue();
-//	    zo_r_x = ((Long) data.get("ZO_R_x")).intValue();
-//	    zo_r_y = ((Long) data.get("ZO_R_y")).intValue();
-//	    zc_g_x = ((Long) data.get("ZC_G_x")).intValue(); 
-//	    zc_g_y = ((Long) data.get("ZC_G_y")).intValue(); 
-//	    zo_g_x = ((Long) data.get("ZO_G_x")).intValue();
-//	    zo_g_y = ((Long) data.get("ZO_G_y")).intValue();
-//         sh_ll_x = ((Long) data.get("SH_LL_x")).intValue();
-//	     sh_ll_y = ((Long) data.get("SH_LL_y")).intValue();
-//	     sh_ur_x = ((Long) data.get("SH_UR_x")).intValue();
-//	     sh_ur_y = ((Long) data.get("SH_UR_y")).intValue();
-//	     sv_ll_x = ((Long) data.get("SV_LL_x")).intValue();
-//	     sv_ll_y = ((Long) data.get("SV_LL_y")).intValue();
-//	     sv_ur_x = ((Long) data.get("SV_UR_x")).intValue();
-//	     sv_ur_y = ((Long) data.get("SV_UR_y")).intValue();
-//	     sr_ll_x = ((Long) data.get("SR_LL_x")).intValue();
-//	     sr_ll_y = ((Long) data.get("SR_LL_y")).intValue();
-//	     sr_ur_x = ((Long) data.get("SR_UR_x")).intValue(); 
-//	     sr_ur_y = ((Long) data.get("SR_UR_y")).intValue();
-//	     sg_ll_x = ((Long) data.get("SG_LL_x")).intValue();
-//	     sg_ll_y = ((Long) data.get("SG_LL_y")).intValue();
-//	     sg_ur_x = ((Long) data.get("SG_UR_x")).intValue();
-//	     sg_ur_y = ((Long) data.get("SG_UR_y")).intValue();
-//	  }catch (Exception e) {
-//	      System.err.println("Error: " + e.getMessage());
-//	    }
+		// directly updates the variables in GameController with the values from the
+		// server
+		wifi.updateDataPoints();
 
-//
-//	    // Wait until user decides to end program
-//	    Button.waitForAnyPress();
-	  
-	  
-//	  nav.turnTo(1,0);
-//	  Button.waitForAnyPress();
-////	  nav.turnTo(0);
-//	  //Button.waitForAnyPress();
-//	  nav.turnTo(1,1);
-//	  Button.waitForAnyPress();
-//	  nav.turnTo(0,1);
-//	  Button.waitForAnyPress();
-//	  nav.turnTo(1,1);
-//-----------
+		//
+		// // Wait until user decides to end program
+		// Button.waitForAnyPress();
+
+		// nav.turnTo(1,0);
+		// Button.waitForAnyPress();
+		//// nav.turnTo(0);
+		// //Button.waitForAnyPress();
+		// nav.turnTo(1,1);
+		// Button.waitForAnyPress();
+		// nav.turnTo(0,1);
+		// Button.waitForAnyPress();
+		// nav.turnTo(1,1);
+		// -----------
 		lp.start();
 		li.cornerLocalization();
 		Button.waitForAnyPress();
 		lp.killTask();
 
-		nav.travelTo(1,1);
+		nav.travelTo(1, 1);
 		lp.restartTask();
-        li.anyPointLocalization();
-        lp.killTask();
-        
-        odo.setX(1*CaptureFlag.TILE_LENGTH);
-        odo.setY(1*CaptureFlag.TILE_LENGTH);
-        
+		li.anyPointLocalization();
+		lp.killTask();
+
+		odo.setX(1 * CaptureFlag.TILE_LENGTH);
+		odo.setY(1 * CaptureFlag.TILE_LENGTH);
+
 		nav.travelTo(0, 2);
-		  lp.restartTask();
-	        li.anyPointLocalization();
-	        lp.killTask();
-//--------------------------
-	        
-	        
-	        
-//    	rightMotor.forward();
-//    	leftMotor.forward();
-//    	zip.setSpeed(150);
-//    	zip.forward();
-//		leftMotor.rotate(CaptureFlag.convertDistance( CaptureFlag.WHEEL_RADIUS, 8), true);
-//	    rightMotor.rotate(CaptureFlag.convertDistance( CaptureFlag.WHEEL_RADIUS, 8), false);
-//		lp.restartTask();
-//		li.anyPointLocalization();
-//		lp.killTask();
-//		rightMotor.endSynchronization();		
+		lp.restartTask();
+		li.anyPointLocalization();
+		lp.killTask();
+		// --------------------------
+
+		// rightMotor.forward();
+		// leftMotor.forward();
+		// zip.setSpeed(150);
+		// zip.forward();
+		// leftMotor.rotate(CaptureFlag.convertDistance( CaptureFlag.WHEEL_RADIUS, 8),
+		// true);
+		// rightMotor.rotate(CaptureFlag.convertDistance( CaptureFlag.WHEEL_RADIUS, 8),
+		// false);
+		// lp.restartTask();
+		// li.anyPointLocalization();
+		// lp.killTask();
+		// rightMotor.endSynchronization();
 	}
-	
+
 }
